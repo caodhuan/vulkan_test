d751f42830134c1424b88d70f907f3080ae61d0d
caodhuan <cao.d.huan@gmail.com>
2023-11-07 14:52:00 +0800

    define vertex in cpp

---
+++ VertexApplication.cpp
@@ -0,0 +1,5 @@
+#include "VertexApplication.h"
+
+VertexApplication::VertexApplication() {}
+
+VertexApplication::~VertexApplication() {}
---
+++ VertexApplication.h
@@ -0,0 +1,12 @@
+// 将结构定义在 cpp 里
+#pragma once
+
+#include "ApplicationBase.h"
+
+class VertexApplication : public ApplicationBase {
+ public:
+  VertexApplication();
+  ~VertexApplication();
+
+ public:
+};
